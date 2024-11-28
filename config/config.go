b36e4a95d0e8274acece3f4a290fe7764f41e098
Lian <lian-rr@users.noreply.github.com>
2024-11-28 16:50:23 -0500

    Config file support (#10)
    deprecating env variables for passing configuration changes

--- config/config.go
+++ config/config.go
@@ -1,47 +1,95 @@
 package config
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"io/fs"
-	"log/slog"
 	"os"
+
+	"github.com/BurntSushi/toml"
 )
+
+// ErrNoConfigFound config file not found
+var ErrNoConfigFound = errors.New("config file or not found")
 
 // App holds the application configuration
 type App struct {
-	BasePath  string
-	Professor ProfessorConfig
+	PathOverride string `toml:"pathOverride"`
+	Debug        bool   `toml:"debug"`
+	Professor    ProfessorConfig
 }
 
-// ErrInvalidDir indicates that the root directory for the store is not valid.
-var ErrInvalidDir = errors.New("invalid base directory")
-
-// New returns a new app.
-func New(ctx context.Context, path string, logger *slog.Logger) (App, error) {
-	if path == "" {
-		def, err := os.UserHomeDir()
+// New returns a new app's config.
+func New(configPath string) (App, error) {
+	if configPath == "" {
+		home, err := os.UserHomeDir()
 		if err != nil {
 			return App{}, err
 		}
-		path = def
+		configPath = fmt.Sprintf("%s/.config/clio", home)
 	}
 
-	path = fmt.Sprintf("%s/.clio", path)
+	cfg, err := loadConfig(configPath)
+	if err != nil {
+		return App{}, err
+	}
+
+	if err := cfg.prepateDataDir(); err != nil {
+		return App{}, err
+	}
+
+	if err := cfg.validate(); err != nil {
+		return App{}, err
+	}
+
+	return cfg, nil
+}
+
+// NewDefault returns a default App.
+func NewDefault() (App, error) {
+	var cfg App
+	err := cfg.prepateDataDir()
+	if err != nil {
+		return App{}, err
+	}
+
+	return cfg, nil
+}
+
+func loadConfig(path string) (App, error) {
+	var cfg App
+	_, err := toml.DecodeFile(fmt.Sprintf("%s/clio.toml", path), &cfg)
+	if err != nil {
+		return App{}, fmt.Errorf("%w path=%q: %v", ErrNoConfigFound, path, err)
+	}
+	return cfg, nil
+}
+
+func (a App) prepateDataDir() error {
+	path := a.GetPath()
 	err := os.Mkdir(path, 0o740)
 	if err != nil && !errors.Is(err, fs.ErrExist) {
-		return App{}, fmt.Errorf("%w: %w", ErrInvalidDir, err)
+		return fmt.Errorf("error preparing the data dir (path=%s): %v", path, err)
 	}
 
-	app := App{
-		BasePath: path,
+	return nil
+}
+
+func (a App) validate() error {
+	var errs error
+
+	if err := a.Professor.validate(); err != nil {
+		errs = errors.Join(errs, err)
 	}
 
-	if err := app.loadProfessor(); err != nil {
-		logger.Error("error loading professor config", slog.Any("error", err))
+	return errs
+}
+
+func (a App) GetPath() string {
+	path := a.PathOverride
+	if path == "" {
+		path, _ = os.UserHomeDir()
 	}
 
-	logger.Info("application setup completed", slog.String("path", path))
-	return app, nil
+	return fmt.Sprintf("%s/.clio", path)
 }
